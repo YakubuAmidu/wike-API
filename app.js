192f98dec2e8b457cd06b709d312f17ace0688dd
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-30 00:10:14 -0700

    Wiki-API

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view-engine', 'ejs');
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static('public'));
+const port = 3000;
+mongoose.connect('mongodb://localhost:27017/wikiDB', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model('Article', articleSchema);
+
+app.get('/articles', function (req, res) {
+  Article.find(function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(port, function () {
+  console.log(`Sever started on port ${port}`);
+});
