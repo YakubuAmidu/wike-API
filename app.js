4cd2bf0c0359092958cd6ee852037b4844497ee7
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-30 00:46:29 -0700

    Initial commit

--- app.js
+++ app.js
@@ -34,6 +34,23 @@ app.get('/articles', function (req, res) {
   });
 });
 
+app.post('/articles', function (req, res) {
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newArticle.save(function (err) {
+    if (!err) {
+      res.send('Successfully added an articles');
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(port, function () {
   console.log(`Sever started on port ${port}`);
 });
